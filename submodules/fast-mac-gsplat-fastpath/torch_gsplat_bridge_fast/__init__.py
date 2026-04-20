fb36657f6eed5de550e1cb13aa3cead38f9b639f
Nicholas Bardy <nicholasbardy@gmail.com>
2026-04-20 23:00:47 +0700

    Update Taichi Mac docs pointer

--- submodules/fast-mac-gsplat-fastpath/torch_gsplat_bridge_fast/__init__.py
+++
@@ -1,2 +0,0 @@
-from .rasterize import RasterConfig, ProjectedGaussianRasterizer, rasterize_projected_gaussians
-__all__ = ["RasterConfig", "ProjectedGaussianRasterizer", "rasterize_projected_gaussians"]
